c55c6d9ac2ac43be43a99d8c9adb1afb9fa258ca
Claude Chibout <claude.chibout@ens.uvsq.fr>
2021-02-09 09:27:43 +0100

    test32

--- incendie.py
+++ incendie.py
@@ -1,6 +1,6 @@
 #########################################
 # groupe  2 MPCI 6
-# Claude Chibout
+# Claude efuhgohh
 # Cyril CLOVIS
 # Dylan THUILLIER
 # Abdel karim BOURAOUI
@@ -8,4 +8,4 @@
 # Olivier GABRIEL
 # https://github.com/uvsq22005256/projet_incendie
 #########################################
-print(a)
+print("blblleokfegruhg")
