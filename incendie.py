a01af7d982e2e749995a04c35e4e1a51a219c0de
Claude Chibout <claude.chibout@ens.uvsq.fr>
2021-02-09 08:46:28 +0100

    Début

---
+++ incendie.py
@@ -0,0 +1,10 @@
+#########################################
+# groupe MPCI 6
+# Claude Chibout
+# Cyril CLOVIS
+# Dylan THUILLIER
+# Abdel karim BOURAOUI
+# Ahmadou bamba SOUM
+# Olivier GABRIEL
+# https://github.com/uvsq22005256/projet_incendie
+#########################################
