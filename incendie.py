7377472c10392b89544e873e44e4b933008aff93
Dylan THUILLIER <dylan.thuillier@ens.uvsq.fr>
2021-03-12 11:00:44 +0100

    Interface graphique et quadrillage

--- incendie.py
+++ incendie.py
@@ -9,11 +9,44 @@
 # https://github.com/uvsq22005256/projet_incendie
 # https://discord.gg/fNQpuPM3yQ
 #########################################
+#Import des fonctions
+import tkinter as tk
+
+#########################################
 #Variable
+largeur = 500
+hauteur = 500
+couleur_canv= 'grey30'
+couleur_quadr = 'grey60'
+cote = 20
+root = tk.Tk()
+canvas = tk.Canvas(root, bg=couleur_canv, width = largeur, height=hauteur)
+
 
 #########################################
 #Fonction
+def quadrillage() :
+ """Dessine un quadrillage formé de carrés de côté Cote"""
+y = 0
+while y <= hauteur :
+    canvas.create_line((0,y), (largeur,y), fill=couleur_quadr)
+    y += cote
+x = 0    
+while x <= largeur :
+    canvas.create_line((x,0), (x, hauteur), fill=couleur_quadr)
+    x += cote
 
 
 #########################################
 #Programme principal
+root.title('Projet Incendie')
+quadrillage()
+
+
+
+
+
+#########################################
+#Création des widgets
+canvas.grid()
+root.mainloop()
